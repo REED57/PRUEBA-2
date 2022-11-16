0e5102407b4dc5f0faedd95dc43e797f14bf767a
invitado <invitado@LAB050420.itchii.lab>
2022-11-16 12:51:59 -0700

    ha

--- src/prueba/pkg2/PRUEBA2.java
+++ src/prueba/pkg2/PRUEBA2.java
@@ -18,6 +18,8 @@ public class PRUEBA2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
+        
+        System.out.println("adios mundo");
     }
     
 }
